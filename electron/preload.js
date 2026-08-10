b5b0e72d299e66d914c8488567cd25c78fb2ebc7
ranjitacharya13-crypto <232028569+ranjitacharya13-crypto@users.noreply.github.com>
2026-08-10 11:57:21 +0000

    Revert "Merge pull request #23 from ranjitacharya13-crypto/arena/019feb75-edunova-x"
    This reverts commit e17ae80d62e0dc92504f0b954fad5bc330658eaa.

--- electron/preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-contextBridge.exposeInMainWorld('edunova', {
-  install: scope => ipcRenderer.invoke('install', scope),
-  saveConfig: values => ipcRenderer.invoke('save-config', values),
-  launch: () => ipcRenderer.invoke('launch'),
-  hasConfig: () => ipcRenderer.invoke('has-config'),
-  onLog: callback => ipcRenderer.on('log', (_event, value) => callback(value))
-});
--- frontend/android/app/src/main/java/com/edunova/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.edunova.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
